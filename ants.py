a583ed605046f1f520806639e23ef8962051b58f
mimir-d <mimir@mail.bg>
2016-09-07 17:37:18 +0300

    g9: extract CircleSprite base entity type

--- ants.py
+++ ants.py
@@ -1,6 +1,3 @@
-# db csv
-# gui pygame?
-# proto ssl socket?
 
 # objects:
 #   1. map 600x600
@@ -26,53 +23,54 @@ import cocos.actions as act
 from cocos.director import director
 
 
-class Player(Sprite):
+class CircleSprite(Sprite):
+    def __init__(self, size, color):
+        super(CircleSprite, self).__init__(self.__create_image(size, color))
+
+    def __create_image(self, size, color):
+        im = Image.new('RGBA', (size, size), (255, 255, 255, 0))
+        draw = ImageDraw.Draw(im)
+        draw.ellipse((1, 1, im.size[0]-1, im.size[1]-1), fill=color)
+
+        im = self._manip_image(im)
+        return ImageData(*im.size, 'RGBA', im.tobytes(), pitch=-im.size[0]*4)
+
+    def _manip_image(self, im):
+        return im
+
+
+class Player(CircleSprite):
     __SIZE = 16
 
     def __init__(self, name):
-        super(Player, self).__init__(self.__create_image(name))
+        self.name = name[:10]
+        super(Player, self).__init__(self.__SIZE, self.__gen_rand_color())
 
     def __gen_rand_color(self):
         array = [random.random() for _ in range(3)]
         r = max(array)
         return tuple(int(255 * i/r) for i in array) + (200,)
 
-    def __create_image(self, name):
-        bg_color = (255, 255, 255, 0)
-        color = self.__gen_rand_color()
+    def _manip_image(self, im):
         font = ImageFont.truetype('assets/arial.ttf', 13)
-
-        # create blob
-        im = Image.new('RGBA', (self.__SIZE, self.__SIZE), bg_color)
         draw = ImageDraw.Draw(im)
-        draw.ellipse((1, 1, im.size[0]-1, im.size[1]-1), fill=color)
+        text_size = draw.textsize(self.name, font=font)
 
         # create name
-        text_size = draw.textsize(name[:10], font=font)
-        im_name = Image.new('RGBA', (im.size[0] + text_size[0], im.size[1] + text_size[1]), bg_color)
+        im_name = Image.new('RGBA', (im.size[0] + text_size[0], im.size[1] + text_size[1]), (255, 255, 255, 0))
         im_name.paste(im, (0, text_size[1]))
 
         draw = ImageDraw.Draw(im_name)
-        draw.text((self.__SIZE, 0), name, font=font, fill=color[:3])
+        draw.text((im.size[0], 0), self.name, font=font, fill=self.color[:3])
 
-        return ImageData(*im_name.size, 'RGBA', im_name.tobytes(), pitch=-im_name.size[0]*4)
+        return im_name
 
 
-class Feeder(Sprite):
+class Feeder(CircleSprite):
     __SIZE = 60
 
     def __init__(self):
-        super(Feeder, self).__init__(self.__create_img())
-
-    def __create_img(self):
-        im = Image.new('RGBA', (self.__SIZE, self.__SIZE), (255, 255, 255, 0))
-
-        draw = ImageDraw.Draw(im)
-        draw.ellipse((1, 1, im.size[0]-1, im.size[1]-1), fill=(128, 255, 128, 240))
-
-        return ImageData(*im.size, 'RGBA', im.tobytes(), pitch=-im.size[0]*4)
-
-    def center(self):
+        super(Feeder, self).__init__(self.__SIZE, (128, 255, 128, 240))
         self.position = 300, 300
 
 
@@ -82,7 +80,7 @@ class Main(ColorLayer):
 
         for i in range(20):
             sprite = Player('1234567890')
-            self.add(sprite)
+            self.add(sprite, z=1)
 
             sprite.position = random.randrange(150, 600), random.randrange(150,600)
             left = act.MoveBy((-150, 0), 2)
@@ -92,7 +90,6 @@ class Main(ColorLayer):
 
     def __init_map(self):
         feeder = Feeder()
-        feeder.center()
         self.add(feeder, z=-1)
 
 

656c5fdbf6b008196bf59e70fb039ace803059ee
mimir-d <mimir@mail.bg>
2016-09-07 18:07:50 +0300

    g9: add physics collision infrastructure

--- ants.py
+++ ants.py
@@ -16,16 +16,18 @@ from PIL import Image, ImageDraw, ImageFont
 from pyglet.image import ImageData
 
 import cocos
-from cocos.layer import Layer, ColorLayer
-from cocos import layer
-from cocos.sprite import Sprite
 import cocos.actions as act
+import cocos.euclid as eu
+import cocos.collision_model as cm
+from cocos.layer import Layer, ColorLayer
+from cocos.sprite import Sprite
 from cocos.director import director
 
 
-class CircleSprite(Sprite):
+class CircleEntity(Sprite):
     def __init__(self, size, color):
-        super(CircleSprite, self).__init__(self.__create_image(size, color))
+        super(CircleEntity, self).__init__(self.__create_image(size, color))
+        self.__init_phys(size)
 
     def __create_image(self, size, color):
         im = Image.new('RGBA', (size, size), (255, 255, 255, 0))
@@ -38,9 +40,15 @@ class CircleSprite(Sprite):
     def _manip_image(self, im):
         return im
 
+    def __init_phys(self, size):
+        self.cshape = cm.CircleShape(eu.Vector2(0, 0), size/2)
 
-class Player(CircleSprite):
-    __SIZE = 24
+    def update(self):
+        self.cshape.center = eu.Vector2(*self.position)
+
+
+class Player(CircleEntity):
+    __SIZE = 25
 
     def __init__(self, name):
         self.name = name[:10]
@@ -65,47 +73,95 @@ class Player(CircleSprite):
 
         return im_name
 
+    def __str__(self):
+        return self.name
 
-class Feeder(CircleSprite):
+
+class Feeder(CircleEntity):
     __SIZE = 80
 
     def __init__(self):
         super(Feeder, self).__init__(self.__SIZE, (128, 255, 128, 240))
 
 
-class Food(CircleSprite):
+class Food(CircleEntity):
     __SIZE = 12
 
     def __init__(self):
         super(Food, self).__init__(self.__SIZE, (255, 255, 0, 200))
+        # TODO:
+        self.alive = True
 
 
 class Main(ColorLayer):
+    WIDTH = 800
+    HEIGHT = 800
+
     def __init__(self):
         super(Main, self).__init__(52, 152, 219, 70)
-
-        for i in range(20):
-            sprite = Player('1234567890')
-            self.add(sprite, z=1)
-
-            sprite.position = random.randrange(150, 800), random.randrange(150,800)
-            left = act.MoveBy((-150, 0), 2)
-            sprite.do(act.Repeat(left + act.Reverse(left)))
-
-        for i in range(10):
-            food = Food()
-            self.add(food, z=1)
-
-            food.position = random.randrange(10, 790), random.randrange(10, 790)
+        self.collision_manager = cm.CollisionManagerBruteForce()
 
         self.__init_map()
+        self.__init_players()
+        self.__init_food()
+
+        self.schedule(self.__update)
+
+    def add(self, obj, *args, **kwargs):
+        super(Main, self).add(obj, *args, **kwargs)
+        self.collision_manager.add(obj)
 
     def __init_map(self):
-        feeder = Feeder()
-        feeder.position = 400, 400
-        self.add(feeder, z=-1)
+        self.feeder = Feeder()
+        self.feeder.position = self.WIDTH//2, self.HEIGHT//2
+        self.add(self.feeder, z=-1)
+
+    def __init_players(self):
+        self.__players = []
+
+        p1 = Player('1')
+        p2 = Player('2')
+        p1.position = 10, 10
+        p2.position = 100, 100
+
+        mv = act.MoveBy((100, 100), 5)
+        p1.do(act.Repeat(mv + act.Reverse(mv)))
+        p2.do(act.Repeat(act.Reverse(mv) + mv))
+
+        self.__players.append(p1)
+        self.__players.append(p2)
+
+        self.add(p1)
+        self.add(p2)
+        # for i in range(20):
+        #     p = Player('123')
+        #     self.__players.append(p)
+
+        #     self.add(p, z=1)
+
+        #     # TODO:
+        #     p.position = random.randrange(150, 800), random.randrange(150, 800)
+        #     left = act.MoveBy((-150, 0), 2)
+        #     sprite.do(act.Repeat(left + act.Reverse(left)))
+
+    def __init_food(self):
+        # for i in range(10):
+        #     food = Food()
+        #     self.add(food, z=1)
+
+        #     food.position = random.randrange(10, 790), random.randrange(10, 790)
+        pass
+
+    def __update(self, dt):
+        self.feeder.update()
+
+        for p in self.__players:
+            p.update()
+
+        for c1, c2 in self.collision_manager.iter_all_collisions():
+            print(c1, 'with', c2, 'at time', dt)
 
 
 if __name__ == '__main__':
-    director.init(width=800, height=800, autoscale=True, resizable=True)
+    director.init(width=Main.WIDTH, height=Main.HEIGHT, autoscale=True, resizable=True)
     director.run(cocos.scene.Scene(Main()))

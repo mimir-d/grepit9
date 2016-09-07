aa295148227819adcd70748a24ad50c1857e1101
mimir-d <mimir@mail.bg>
2016-09-07 16:18:25 +0300

    g9: create Feeder

--- ants.py
+++ ants.py
@@ -27,16 +27,15 @@ from cocos.director import director
 
 
 class Player(Sprite):
-    __PLAYER_SIZE = 16
+    __SIZE = 16
 
-    def __init__(self, name, *args, **kwargs):
-        super(Player, self).__init__(self.__create_image(name), *args, **kwargs)
+    def __init__(self, name):
+        super(Player, self).__init__(self.__create_image(name))
 
     def __gen_rand_color(self):
         array = [random.random() for _ in range(3)]
         r = max(array)
         return tuple(int(255 * i/r) for i in array) + (200,)
-
 
     def __create_image(self, name):
         bg_color = (255, 255, 255, 0)
@@ -44,7 +43,7 @@ class Player(Sprite):
         font = ImageFont.truetype('assets/arial.ttf', 13)
 
         # create blob
-        im = Image.new('RGBA', (self.__PLAYER_SIZE, self.__PLAYER_SIZE), bg_color)
+        im = Image.new('RGBA', (self.__SIZE, self.__SIZE), bg_color)
         draw = ImageDraw.Draw(im)
         draw.ellipse((1, 1, im.size[0]-1, im.size[1]-1), fill=color)
 
@@ -54,15 +53,32 @@ class Player(Sprite):
         im_name.paste(im, (0, text_size[1]))
 
         draw = ImageDraw.Draw(im_name)
-        draw.text((self.__PLAYER_SIZE, 0), name, font=font, fill=color)
+        draw.text((self.__SIZE, 0), name, font=font, fill=color[:3])
 
         return ImageData(*im_name.size, 'RGBA', im_name.tobytes(), pitch=-im_name.size[0]*4)
 
 
+class Feeder(Sprite):
+    __SIZE = 60
+
+    def __init__(self):
+        super(Feeder, self).__init__(self.__create_img())
+
+    def __create_img(self):
+        im = Image.new('RGBA', (self.__SIZE, self.__SIZE), (255, 255, 255, 0))
+
+        draw = ImageDraw.Draw(im)
+        draw.ellipse((1, 1, im.size[0]-1, im.size[1]-1), fill=(128, 255, 128, 240))
+
+        return ImageData(*im.size, 'RGBA', im.tobytes(), pitch=-im.size[0]*4)
+
+    def center(self):
+        self.position = 300, 300
+
+
 class Main(ColorLayer):
     def __init__(self):
-        # super(Main, self).__init__(52//2, 152//2, 219//2, 1000)
-        super(Main, self).__init__(30, 50, 70, 1000)
+        super(Main, self).__init__(52, 152, 219, 70)
 
         for i in range(20):
             sprite = Player('1234567890')
@@ -72,7 +88,14 @@ class Main(ColorLayer):
             left = act.MoveBy((-150, 0), 2)
             sprite.do(act.Repeat(left + act.Reverse(left)))
 
+        self.__init_map()
+
+    def __init_map(self):
+        feeder = Feeder()
+        feeder.center()
+        self.add(feeder, z=-1)
 
 
-director.init(width=600, height=600, autoscale=True, resizable=True)
-director.run(cocos.scene.Scene(Main()))
+if __name__ == '__main__':
+    director.init(width=600, height=600, autoscale=True, resizable=True)
+    director.run(cocos.scene.Scene(Main()))
